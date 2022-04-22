5281cef0d8d61068b9b3cc387b757610fcb17261
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2022-04-22 10:54:11 -0400

    minor updates to auto extract filename from config

--- docker-build.sh
+++ docker-build.sh
@@ -2,24 +2,25 @@
 
 set -e
 
+TARBALL_FOLDER="${HOME}/development/openwrt/bin/targets/x86/64/"
 DISTRO=openwrt
-BUILD_VERSION=4.2.1
-TARBALL="rfhs-rfctf-${BUILD_VERSION}-2021.08.04-public-openwrt-image-x86-64-generic-rootfs.tar.gz"
-cp ~/development/openwrt/bin/targets/x86/64/"${TARBALL}" .
+BUILD_VERSION_NUMBER="$(awk -F'"' '/CONFIG_VERSION_NUMBER/ {print $2}' "${TARBALL_FOLDER}/../../../../.config" )"
+BUILD_VERSION_CODE="$(awk -F'"' '/CONFIG_VERSION_CODE=/ {gsub(" ", "-"); print tolower($2)}' "${TARBALL_FOLDER}/../../../../.config" )"
+TARBALL="rfhs-rfctf-${BUILD_VERSION_NUMBER}-${BUILD_VERSION_CODE}-x86-64-generic-rootfs.tar.gz"
+cp "${TARBALL_FOLDER}${TARBALL}" .
 
 CI_REGISTRY_IMAGE=rfhs
 BUILD_DATE=$(date -u +"%Y.%m.%d")
 
 IMAGE=$DISTRO
-VERSION=$BUILD_VERSION
 
-docker build --pull -t "${CI_REGISTRY_IMAGE}/${IMAGE}:${VERSION}" \
-    --build-arg TARBALL=${TARBALL} \
-    --build-arg BUILD_DATE=${BUILD_DATE} \
-    --build-arg VERSION=${VERSION} \
+docker build -t "${CI_REGISTRY_IMAGE}/${IMAGE}:${BUILD_VERSION_NUMBER}" \
+    --build-arg TARBALL="${TARBALL}" \
+    --build-arg BUILD_DATE="${BUILD_DATE}" \
+    --build-arg VERSION="${BUILD_VERSION_NUMBER}" \
     .
 
-docker tag "${CI_REGISTRY_IMAGE}/${IMAGE}:${VERSION}" "${CI_REGISTRY_IMAGE}/${IMAGE}:latest"
-docker push "${CI_REGISTRY_IMAGE}/${IMAGE}:${VERSION}"
+docker tag "${CI_REGISTRY_IMAGE}/${IMAGE}:${BUILD_VERSION_NUMBER}" "${CI_REGISTRY_IMAGE}/${IMAGE}:latest"
+docker push "${CI_REGISTRY_IMAGE}/${IMAGE}:${BUILD_VERSION_NUMBER}"
 docker push "${CI_REGISTRY_IMAGE}/${IMAGE}:latest"
 rm "${TARBALL}"
