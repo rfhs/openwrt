3c0649152256202a395deae8c9c498577dc84e8d
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2022-09-15 21:33:55 -0400

    huge sync with defcon/circlecity updates
    I did a lot of crazy stuff to make defcon work, and here it all is,
    including the fixes that make it reasonably sane.  rfhs_predictable_phys
    renames the phys based on capability so the right phys are always
    running the right SSIDs.  This basically doesn't matter for the virtual
    game, but it very much is required to run in meatspace with
    heterogeneous hardware.

--- docker-build.sh
+++ docker-build.sh
@@ -2,6 +2,59 @@
 
 set -eux
 
+rfkill_check() {
+	#take phy and check blocks
+	if [ -z "${1}" ]; then
+		printf "Fatal, rfkill_check requires a phy to be passed in\n"
+		exit 1
+	fi
+	#first we have to find the rfkill index
+	#this is available as /sys/class/net/wlan0/phy80211/rfkill## but that's a bit difficult to parse
+	index="$(sudo rfkill list | grep "${1}:" | head -n1 | awk -F: '{print $1}')"
+	if [ -z "$index" ]; then
+		return 187
+	fi
+	rfkill_status="$(sudo rfkill list "${index}" 2>&1)"
+	if [ $? != 0 ]; then
+		printf "rfkill error: %s\n" "${rfkill_status}"
+		return 187
+	elif [ -z "${rfkill_status}" ]; then
+		printf "rfkill had no output, something went wrong.\n"
+		exit 1
+	else
+		soft=$(printf "%s" "${rfkill_status}" | grep -i soft | awk '{print $3}')
+		hard=$(printf "%s" "${rfkill_status}" | grep -i hard | awk '{print $3}')
+		if [ "${soft}" = "yes" ] && [ "${hard}" = "no" ]; then
+			return 1
+		elif [ "${soft}" = "no" ] && [ "${hard}" = "yes" ]; then
+			return 2
+		elif [ "${soft}" = "yes" ] && [ "${hard}" = "yes" ]; then
+			return 3
+		fi
+	fi
+	return 0
+}
+
+rfkill_unblock() {
+	#attempt unblock and CHECK SUCCESS
+	#rfkill_status="$(sudo rfkill unblock "${1#phy}" 2>&1)"
+	#if [ $? != 0 ]; then
+		rfkill_status="$(sudo rfkill unblock "${index}" 2>&1)"
+		if [ $? != 0 ]; then
+      if [ "$(printf "%s" "${rfkill_status}" | grep -c "Usage")" -eq 1 ]; then
+				printf "Missing parameters in rfkill! Report this"
+			else
+				printf "rfkill error: %s\n" "${rfkill_status}"
+			fi
+			printf "Unable to unblock.\n"
+			return 1
+		fi
+	#fi
+
+	sleep 1
+	return 0
+}
+
 TARBALL_FOLDER="${HOME}/development/openwrt/bin/targets/x86/64/"
 DISTRO=openwrt
 BUILD_VERSION_NUMBER="$(awk -F'"' '/CONFIG_VERSION_NUMBER/ {print $2}' "${TARBALL_FOLDER}/../../../../.config" )"
@@ -53,15 +106,14 @@ if [ -n "$(docker ps -a -q)" ]; then
 fi
 
 # Get a list of the radios (a little safer than assuming)
-#CONTAINER_PHYS="$(sudo airmon-ng | awk '/mac80211_hwsim/ {print $1}')"
-CONTAINER_PHYS="phy8 phy1 phy2 phy3 phy4 phy5 phy6 phy7"
+#CONTAINER_PHYS="phy8 phy1 phy2 phy3 phy4 phy5 phy6 phy7"
 CONTAINER_PHYS="$(sudo airmon-ng | awk '/mac80211_hwsim/ {print $1}')"
 # Start the container
 docker run -d --rm --network none --name "${CONTAINER_NAME}" \
   --tmpfs /run:mode=0755,nosuid,nodev,nr_inodes=800k,size=20%,strictatime \
   --tmpfs /tmp:mode=0755,nosuid,nodev,nr_inodes=800k,size=20% \
-  $(if [ -d '/home/zero/development/rfhs/wctf-restricted' ]; then find '/home/zero/development/rfhs/wctf-restricted/wifi/openwrt/airkraken/files' -type f -exec printf "-v %s:%s\n" "{}" "{}" \; | sed 's#:/home/zero/development/rfhs/wctf-restricted/wifi/openwrt/airkraken/files#:#;s#$#:ro#'; fi) \
   --cap-add net_raw --cap-add net_admin \
+  $(if [ -d '/home/zero/development/rfhs/wctf-restricted' ]; then find '/home/zero/development/rfhs/wctf-restricted/wifi/openwrt/airkraken/files' -type f -exec printf "-v %s:%s\n" "{}" "{}" \; | sed 's#:/home/zero/development/rfhs/wctf-restricted/wifi/openwrt/airkraken/files#:#;s#$#:ro#'; fi) \
   "${CI_REGISTRY_IMAGE}/${IMAGE}:${BUILD_VERSION_NUMBER}"
   #--security-opt seccomp=unconfined \
 # Give it radios
@@ -82,15 +134,17 @@ for phy in ${CONTAINER_PHYS}; do
     printf "Unable to find %s, waiting...\n" "${phy}"
     sleep 1
   done
+  rfkill_check "${phy}" || rfkill_unblock "${phy}"
+  sleep 1
   sudo iw phy "${phy}" set netns "${clientpid}"
 done
 sleep 10
 if docker exec "${CONTAINER_NAME}" /usr/sbin/rfhs_checker; then
   docker stop "${CONTAINER_NAME}"
   sudo modprobe -r mac80211_hwsim
-  #docker tag "${CI_REGISTRY_IMAGE}/${IMAGE}:${BUILD_VERSION_NUMBER}" "${CI_REGISTRY_IMAGE}/${IMAGE}:latest"
-  #docker push "${CI_REGISTRY_IMAGE}/${IMAGE}:${BUILD_VERSION_NUMBER}"
-  #docker push "${CI_REGISTRY_IMAGE}/${IMAGE}:latest"
+  docker tag "${CI_REGISTRY_IMAGE}/${IMAGE}:${BUILD_VERSION_NUMBER}" "${CI_REGISTRY_IMAGE}/${IMAGE}:latest"
+  docker push "${CI_REGISTRY_IMAGE}/${IMAGE}:${BUILD_VERSION_NUMBER}"
+  docker push "${CI_REGISTRY_IMAGE}/${IMAGE}:latest"
   exit_code=0
 else
   printf "rfhs_checker failed!\n"
